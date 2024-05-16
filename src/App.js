09fe3e297b83a5136a30fbc342d0a87ca4367349
Henrique Luis Furman <engs-henriquefurman@camporeal.edu.br>
2024-05-15 21:51:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import './estilo.css';
+
 import biscoito from './assets/biscoito.png';
 
 class App extends Component{
-  
+
   constructor(props){
     super(props);
     this.state = {
-      TextoFrase: ''
+      textoFrase: ''
     };
     this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
-    
-    this.frases = ['Siga os bons e aprenda com eles',
-    'O bom-senso vale mais do que muito conhecimento', 
-    'o riso é a menor distancia entre duas pessoas', 
-    'deixe de lado as preocupaçoes e seja feliz', 
-    'realize o obivio, pense no improvavel e conquiste o impossivel', 
-    'Acredite em milagres, mais nao dependa deles', 
-    'a maior barreira para o sucesso é o medo do fracasso'];
+
+    this.frases = ['Siga os bons e aprenda com eles','O bom-senso vale mais do que muito conhecimento','O riso é a menor distância entre duas pessoas','Deixe de lado as preocupações e seja feliz','Realize o óbvio, pense no improvável e conquiste o impossível','Acredite em milagres, mas não dependa deles','A maior barreira para o sucesso é o medo do fracasso'];
   }
+
   quebrarBiscoito(){
     let state = this.state;
     let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
-    state.TextoFrase = this.frases[numeroAleatorio];
+    state.textoFrase = this.frases[numeroAleatorio];
     this.setState(state);
   }
+
+  render(){
+    return(
+      <div className='container'>
+        <img src={biscoito} className='img' alt='imagem do biscoito'/>
+        <Botao acao={this.quebrarBiscoito} nome="Abrir Biscoito"/>
+        <h3 className='textoFrase'>{this.state.textoFrase}</h3>
+      </div>
+    );
+  }
 }
+
+class Botao extends Component{
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.acao}>{this.props.nome}</button>
+      </div>
+    );
+  }
+}
+
+export default App;
